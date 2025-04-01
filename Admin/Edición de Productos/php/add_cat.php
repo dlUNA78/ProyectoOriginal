fe35561a052a8078130154276d9b37a56fa0983f
dlUNA78 <dajelu2003@gmail.com>
2025-04-01 13:36:30 -0600

    CAMBIOS JENNI Y LUNA
    Se reemplazaron las extensioones por PHP, se agrega una nueva funcion par agregar datos a la BD, se inserto php para usarlo, cambios pendientes

---
+++ Admin/Edición de Productos/php/add_cat.php
@@ -0,0 +1,19 @@
+<?php
+//Recibe datos del formulario //SE REEEMPLAZA CON EL NOMBRE DE LOS CAMPOS DEL FORMULARIO
+$nombre = $_POST['Nombre'];
+$apellidos = $_POST['Apellidos'];
+//Conexion a la base de datos
+include("conexion.php");
+//Inserta los datos en la tabla
+//SE CAMBIARAN LOS DATOS TAL COMO SE ENCUENTREN EN LA BASE DE DATOS
+$sql = "INSERT INTO categorias (nombre, apellidos) VALUES ('$nombre', '$apellidos')";
+if ($conn->query($sql) === TRUE) {
+    echo "Datos insertados correctamente";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+//Cierra la conexion
+$conn->close();
+
+header("Location: index.php");
+?>
