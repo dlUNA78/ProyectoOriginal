6b34cd9d8318b34e0d727a35ef453f38b1fed83f
EduardoGPT4 <ultraepic12345@gmail.com>
2025-04-04 11:29:19 -0600

    Actualizacion de la db en usuarios
    cambie de email a usuario, cambie de password a contraseña y agregue la columna para imagenes
    y ya

--- database.sql
+++ database.sql
@@ -5,8 +5,9 @@ USE `proyecto1`;
 Create table if no exists `usuarios` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `nombre` VARCHAR(50) NOT NULL,
-    `email` VARCHAR(50) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
+    `usuario` VARCHAR(50) NOT NULL,
+    `contraseña` VARCHAR(255) NOT NULL,
+    `imagen` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
 );
 
