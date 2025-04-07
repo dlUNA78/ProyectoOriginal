47cc02759478cbbb7d0673a57171cd1d3056485d
TrinoTM <gonzalezmadrigaltrinidad@gmail.com>
2025-04-06 21:41:40 -0600

    agregar espacios a pie de pagina

--- Admin/Edición de Productos/add_product.php
+++ Admin/Edición de Productos/add_product.php
@@ -330,7 +330,7 @@
         <div class="container my-auto">
           <div class="text-center my-auto copyright">
             <span><br />TECNM Campus Coalcomán Ingeniería en Sistemas
-              Computacionales 6°Semestre -2025<br /><br /></span>
+              Computacionales 6° Semestre - 2025<br /><br /></span>
           </div>
         </div>
       </footer>
